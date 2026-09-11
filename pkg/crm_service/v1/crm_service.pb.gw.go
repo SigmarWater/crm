5103501222b73cfd29e5dc703f5fba60be870666
SigmaWater <artemudalcov05@gmail.com>
2026-09-11 12:20:17 +0300

    feat: rewrite api

--- pkg/crm_service/v1/crm_service.pb.gw.go
+++ pkg/crm_service/v1/crm_service.pb.gw.go
@@ -175,39 +175,6 @@ func local_request_CRMService_DeleteClient_0(ctx context.Context, marshaler runt
 	return msg, metadata, err
 }
 
-var filter_CRMService_ListClients_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-
-func request_CRMService_ListClients_0(ctx context.Context, marshaler runtime.Marshaler, client CRMServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var (
-		protoReq ListClientsRequest
-		metadata runtime.ServerMetadata
-	)
-	io.Copy(io.Discard, req.Body)
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_CRMService_ListClients_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	msg, err := client.ListClients(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-}
-
-func local_request_CRMService_ListClients_0(ctx context.Context, marshaler runtime.Marshaler, server CRMServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var (
-		protoReq ListClientsRequest
-		metadata runtime.ServerMetadata
-	)
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_CRMService_ListClients_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	msg, err := server.ListClients(ctx, &protoReq)
-	return msg, metadata, err
-}
-
 // RegisterCRMServiceHandlerServer registers the http handlers for service CRMService to "mux".
 // UnaryRPC     :call CRMServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -293,26 +260,6 @@ func RegisterCRMServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux,
 			return
 		}
 		forward_CRMService_DeleteClient_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-	})
-	mux.Handle(http.MethodGet, pattern_CRMService_ListClients_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		var stream runtime.ServerTransportStream
-		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/crm_service.v1.CRMService/ListClients", runtime.WithHTTPPathPattern("/crm/v1/clients"))
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := local_request_CRMService_ListClients_0(annotatedContext, inboundMarshaler, server, req, pathParams)
-		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
-		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
-		if err != nil {
-			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		forward_CRMService_ListClients_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
 
 	return nil
@@ -422,23 +369,6 @@ func RegisterCRMServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux,
 		}
 		forward_CRMService_DeleteClient_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
-	mux.Handle(http.MethodGet, pattern_CRMService_ListClients_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/crm_service.v1.CRMService/ListClients", runtime.WithHTTPPathPattern("/crm/v1/clients"))
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_CRMService_ListClients_0(annotatedContext, inboundMarshaler, client, req, pathParams)
-		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
-		if err != nil {
-			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		forward_CRMService_ListClients_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-	})
 	return nil
 }
 
@@ -447,7 +377,6 @@ var (
 	pattern_CRMService_GetClient_0    = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"crm", "v1", "clients", "uuid"}, ""))
 	pattern_CRMService_UpdateClient_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"crm", "v1", "clients", "uuid"}, ""))
 	pattern_CRMService_DeleteClient_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"crm", "v1", "clients", "uuid"}, ""))
-	pattern_CRMService_ListClients_0  = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"crm", "v1", "clients"}, ""))
 )
 
 var (
@@ -455,5 +384,4 @@ var (
 	forward_CRMService_GetClient_0    = runtime.ForwardResponseMessage
 	forward_CRMService_UpdateClient_0 = runtime.ForwardResponseMessage
 	forward_CRMService_DeleteClient_0 = runtime.ForwardResponseMessage
-	forward_CRMService_ListClients_0  = runtime.ForwardResponseMessage
 )
